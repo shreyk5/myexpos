9ff1f140e736515f43a5cc7bc00dc8617485179f
shreyk5 <kaushik.shrey5@gmail.com>
2019-09-11 19:52:34 +0530

    Doing Stage 19

--- batch.bat
+++ batch.bat
@@ -7,13 +7,13 @@ load --int=9 ../spl/spl_progs/INT9.xsm
 load --int=10 ../spl/spl_progs/INT10.xsm
 load --int=console ../spl/spl_progs/console_interrupt_handler.xsm
 load --int=disk ../spl/spl_progs/disk_handler.xsm
-load --exhandler ../spl/spl_progs/haltprog.xsm
+load --exhandler ../spl/spl_progs/exception_handler.xsm
 load --module 7 ../spl/spl_progs/MOD7.xsm
 load --module 5 ../spl/spl_progs/module5_stage14.xsm
 load --module 0 ../spl/spl_progs/MOD0.xsm
 load --module 4 ../spl/spl_progs/MOD4.xsm
-load --module 1 ../spl/spl_progs/module1_stage17.xsm
-load --module 2 ../spl/spl_progs/module2_stage17.xsm
+load --module 1 ../spl/spl_progs/MOD1.xsm
+load --module 2 ../spl/spl_progs/MOD2.xsm
 load --idle ../expl/samples/idle.xsm
 load --init ../expl/samples/shell_prg1.xsm
-load --exec ../expl/samples/gcd.xsm
+load --exec ../expl/samples/test_ex.xsm
