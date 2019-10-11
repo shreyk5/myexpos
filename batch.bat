b5c37dd3f810364c5f1fb84bd19fae3e2402ff40
Shrey Kaushik <kaushik.shrey5@gmail.com>
2019-10-11 17:19:37 +0530

    Stage 22 completed :)

--- batch.bat
+++ batch.bat
@@ -20,10 +20,12 @@ load --module 4 ../spl/spl_progs/MOD4.xsm
 load --module 1 ../spl/spl_progs/MOD1.xsm
 load --module 2 ../spl/spl_progs/MOD2.xsm
 load --idle ../expl/samples/idle.xsm
-load --init ../expl/samples/fork_s20.xsm
+load --init ../expl/samples/shell_s21.xsm
+load --exec ../expl/samples/pid.xsm
 load --exec ../expl/samples/par_s22.xsm
 load --exec ../expl/samples/Odd.xsm
 load --exec ../expl/samples/Even.xsm
 load --exec ../expl/samples/childS22.xsm
 load --exec ../expl/samples/MS1.xsm
 load --exec ../expl/samples/MS2.xsm
+load --exec ../expl/samples/testf.xsm
