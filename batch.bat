0ebe2b91b03d4f7b6ab5c4964fad43c59bf212bc
Shrey Kaushik <kaushik.shrey5@gmail.com>
2019-10-19 05:19:27 +0530

    Stage 24 done

--- batch.bat
+++ batch.bat
@@ -2,6 +2,7 @@ load --os ../spl/spl_progs/os_startup_final.xsm
 load --library ../expl/library.lib
 load --int=timer ../spl/spl_progs/timer_stage14.xsm
 load --int=4 ../spl/spl_progs/INT4.xsm
+load --int=5 ../spl/spl_progs/INT5.xsm
 load --int=6 ../spl/spl_progs/INT6.xsm
 load --int=7 ../spl/spl_progs/INT7.xsm
 load --int=8 ../spl/spl_progs/INT8.xsm
@@ -17,16 +18,17 @@ load --exhandler ../spl/spl_progs/exception_handler.xsm
 load --module 7 ../spl/spl_progs/MOD7.xsm
 load --module 5 ../spl/spl_progs/module5_stage14.xsm
 load --module 0 ../spl/spl_progs/MOD0.xsm
+load --module 3 ../spl/spl_progs/MOD3.xsm
 load --module 4 ../spl/spl_progs/MOD4.xsm
 load --module 1 ../spl/spl_progs/MOD1.xsm
 load --module 2 ../spl/spl_progs/MOD2.xsm
 load --idle ../expl/samples/idle.xsm
 load --init ../expl/samples/shell_s21.xsm
+load --exec ../expl/samples/asg2_s24.xsm
 load --exec ../expl/samples/pid.xsm
 load --exec ../expl/samples/par_s22.xsm
-load --exec ../expl/samples/Odd.xsm
 load --exec ../expl/samples/Even.xsm
 load --exec ../expl/samples/childS22.xsm
 load --exec ../expl/samples/MS1.xsm
 load --exec ../expl/samples/MS2.xsm
-load --exec ../expl/samples/testf.xsm
+load --exec ../expl/samples/asg1_s24.xsm
