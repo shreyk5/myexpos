cdcefc965e64c6da0a0f909b5a3e5185f7ac9265
Shrey Kaushik <kaushik.shrey5@gmail.com>
2019-10-28 03:04:06 +0530

    Doing stage 27

--- batch.bat
+++ batch.bat
@@ -1,6 +1,6 @@
 load --os ../spl/spl_progs/os_startup_final.xsm
 load --library ../expl/library.lib
-load --int=timer ../spl/spl_progs/timer_stage14.xsm
+load --int=timer ../spl/spl_progs/timer.xsm
 load --int=4 ../spl/spl_progs/INT4.xsm
 load --int=5 ../spl/spl_progs/INT5.xsm
 load --int=6 ../spl/spl_progs/INT6.xsm
@@ -19,7 +19,8 @@ load --int=console ../spl/spl_progs/console_interrupt_handler.xsm
 load --int=disk ../spl/spl_progs/disk_handler.xsm
 load --exhandler ../spl/spl_progs/exception_handler.xsm
 load --module 7 ../spl/spl_progs/MOD7.xsm
-load --module 5 ../spl/spl_progs/module5_stage14.xsm
+load --module 5 ../spl/spl_progs/MOD5.xsm
+load --module 6 ../spl/spl_progs/MOD6.xsm
 load --module 0 ../spl/spl_progs/MOD0.xsm
 load --module 3 ../spl/spl_progs/MOD3.xsm
 load --module 4 ../spl/spl_progs/MOD4.xsm
@@ -39,10 +40,12 @@ load --exec ../expl/samples/asg1_s24.xsm
 load --exec ../expl/samples/asg1_s25.xsm
 load --exec ../expl/samples/asg2_s25.xsm
 load --exec ../expl/samples/asg3_s25.xsm
-load --exec ../expl/samples/ASG3.xsm
 load --exec ../expl/samples/asg4_s25.xsm
 load --exec ../expl/samples/ls.xsm
 load --exec ../expl/samples/rm.xsm
 load --exec ../expl/samples/cp.xsm
 load --exec ../expl/samples/cat.xsm
+load --exec ../expl/samples/asg2_s27.xsm
+load --exec ../expl/samples/asg3_s27.xsm
+load --exec ../expl/samples/pgm1.xsm
 load --data ../sample.dat
