2aea9f3aa098157ff6820ecd57ff3c7a761ab7a7
Shrey Kaushik <kaushik.shrey5@gmail.com>
2019-10-29 03:57:02 +0530

    debugging stage 27:(

--- batch.bat
+++ batch.bat
@@ -22,21 +22,19 @@ load --module 7 ../spl/spl_progs/MOD7.xsm
 load --module 5 ../spl/spl_progs/MOD5.xsm
 load --module 6 ../spl/spl_progs/MOD6.xsm
 load --module 0 ../spl/spl_progs/MOD0.xsm
-load --module 3 ../spl/spl_progs/MOD3.xsm
+load --module 3 ../spl/spl_progs/module_3.xsm
 load --module 4 ../spl/spl_progs/MOD4.xsm
 load --module 1 ../spl/spl_progs/MOD1.xsm
 load --module 2 ../spl/spl_progs/MOD2.xsm
 load --idle ../expl/samples/idle.xsm
 load --init ../expl/samples/Login.xsm
 load --shell ../expl/samples/shell_s26.xsm
-load --exec ../expl/samples/asg2_s24.xsm
 load --exec ../expl/samples/pid.xsm
 load --exec ../expl/samples/par_s22.xsm
 load --exec ../expl/samples/Even.xsm
 load --exec ../expl/samples/childS22.xsm
 load --exec ../expl/samples/MS1.xsm
 load --exec ../expl/samples/MS2.xsm
-load --exec ../expl/samples/asg1_s24.xsm
 load --exec ../expl/samples/asg1_s25.xsm
 load --exec ../expl/samples/asg2_s25.xsm
 load --exec ../expl/samples/asg3_s25.xsm
@@ -47,5 +45,11 @@ load --exec ../expl/samples/cp.xsm
 load --exec ../expl/samples/cat.xsm
 load --exec ../expl/samples/asg2_s27.xsm
 load --exec ../expl/samples/asg3_s27.xsm
+load --exec ../expl/samples/asg4_s27.xsm
 load --exec ../expl/samples/pgm1.xsm
+load --exec ../expl/samples/pgm2.xsm
+load --exec ../expl/samples/merge.xsm
+load --exec ../expl/samples/m_store.xsm
+load --exec ../expl/samples/m_sort.xsm
+load --exec ../expl/samples/m_merge.xsm
 load --data ../sample.dat
