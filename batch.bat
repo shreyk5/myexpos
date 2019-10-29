e51dbbddb53b98fac0f039c42dcbf1d55e52e17f
Shrey Kaushik <kaushik.shrey5@gmail.com>
2019-10-29 19:31:10 +0530

    Stage 27 done

--- batch.bat
+++ batch.bat
@@ -22,7 +22,7 @@ load --module 7 ../spl/spl_progs/MOD7.xsm
 load --module 5 ../spl/spl_progs/MOD5.xsm
 load --module 6 ../spl/spl_progs/MOD6.xsm
 load --module 0 ../spl/spl_progs/MOD0.xsm
-load --module 3 ../spl/spl_progs/module_3.xsm
+load --module 3 ../spl/spl_progs/MOD3.xsm
 load --module 4 ../spl/spl_progs/MOD4.xsm
 load --module 1 ../spl/spl_progs/MOD1.xsm
 load --module 2 ../spl/spl_progs/MOD2.xsm
@@ -52,4 +52,5 @@ load --exec ../expl/samples/merge.xsm
 load --exec ../expl/samples/m_store.xsm
 load --exec ../expl/samples/m_sort.xsm
 load --exec ../expl/samples/m_merge.xsm
+load --exec ../expl/samples/asg1_s27.xsm
 load --data ../sample.dat
